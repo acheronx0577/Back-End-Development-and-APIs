d488704a18ebf79698a4f19c97f4315b6cfe0643
AcheronX. <richardhuynh53@gmail.com>
2025-11-06 19:18:30 -0800

    Fixed bug

--- Basic Node and Express/boilerplate-express/myApp.js
+++ Basic Node and Express/boilerplate-express/myApp.js
@@ -1,39 +1,19 @@
-let express = require('express');
-let app = express();
+require('dotenv').config();
+const express = require('express');
+const app = express();
 
+app.use('/public', express.static(__dirname + '/public'));
 
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
 
+app.get('/json', function(req, res) {
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({"message": "HELLO JSON"});
+  } else {
+    res.json({"message": "Hello json"});
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app; 
